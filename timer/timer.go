4bf24f20693886728e1da8299ca4e2d548a6f0db
alex cai <caiyingyao@ibbd.net>
2016-11-08 10:39:38 +0800

    优化定时器 减少定时器的误差

--- timer/timer.go
+++ timer/timer.go
@@ -1,21 +1,23 @@
 package timer
 
 import (
+	//"fmt"
 	"time"
 )
 
 // 任务对象结构
 type Entry struct {
-	cmd      func()        // 任务函数
-	time     time.Time     // 任务上次执行时间
-	duration time.Duration // 任务执行的时间间隔
+	cmd           func()        // 任务函数
+	duration      time.Duration // 任务执行的时间间隔
+	lastBeginTime time.Time     // 任务上次开始时间
+	lastEndTime   time.Time     // 任务上次结束时间
 }
 
 // 任务队列
-var tasks map[int]*Entry
+var tasks []*Entry
 
 func init() {
-	tasks = make(map[int]*Entry)
+	tasks = make([]*Entry, 0, 8)
 	start()
 }
 
@@ -23,28 +25,53 @@ func init() {
 func AddFunc(cmd func(), duration time.Duration) {
 	entry := &Entry{
 		cmd:      cmd,
-		duration: duration,
+		duration: duration - time.Millisecond, // 减少每次运行时间的误差
 	}
-	lenTasks := len(tasks)
-	tasks[lenTasks] = entry
+	tasks = append(tasks, entry)
 }
 
+// 获取现在时间的函数
+var nowFunc = time.Now
+
+const (
+	// 时钟的周期
+	Duration = time.Millisecond * 200
+)
+
 // 开始执行任务函数
-// 注意: 精度不是很高, 可能会有1秒的延迟, 通常够用
 func start() {
-	timer := time.NewTicker(time.Second)
+	timer := time.NewTicker(Duration)
 	go func() {
 		for {
 			select {
 			case <-timer.C:
-				now := time.Now()
-				for key, entry := range tasks {
-					if entry.time.IsZero() || now.After(entry.time.Add(entry.duration)) {
-						tasks[key].time = now
-						go entry.cmd()
-					}
+				for _, entry := range tasks {
+					go runTask(entry)
 				}
 			}
 		}
 	}()
+}
+
+func runTask(entry *Entry) {
+	//println(entry.duration)
+	//println(entry.lastBeginTime.Format(time.RFC3339), entry.lastEndTime.Format(time.RFC3339))
+	if !entry.lastBeginTime.IsZero() && entry.lastEndTime.IsZero() {
+		// is running
+		//println("return1")
+		return
+	}
+
+	if entry.lastBeginTime.After(entry.lastEndTime) {
+		// is running
+		//println("return2")
+		return
+	}
+
+	if entry.lastBeginTime.IsZero() || nowFunc().After(entry.lastBeginTime.Add(entry.duration)) {
+		entry.lastBeginTime = nowFunc()
+		entry.cmd()
+		entry.lastEndTime = nowFunc()
+		return
+	}
 }
--- timer/timer_test.go
+++ timer/timer_test.go
@@ -7,12 +7,17 @@ import (
 
 func TestTimer(t *testing.T) {
 	AddFunc(func() {
-		println("--->", time.Now().Format("2006-01-02 03:04:05"))
+		println("--->", time.Now().Format(time.RFC3339))
 	}, 5*time.Second)
 
 	AddFunc(func() {
-		println("===>", time.Now().Format("2006-01-02 03:04:05"))
+		println("===>", time.Now().Format(time.RFC3339))
 	}, 10*time.Second)
+
+	AddFunc(func() {
+		println("   >", time.Now().Format(time.RFC3339))
+		time.Sleep(time.Second * 2)
+	}, time.Second)
 
 	time.Sleep(time.Second * 30)
 }
